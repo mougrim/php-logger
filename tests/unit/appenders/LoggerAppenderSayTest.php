7762a32a5d66760d591bc8983912c0e48a31fe10
mitallast <mitallast@gmail.com>
2013-06-16 12:44:04 +0400

    fix test say

--- tests/unit/appenders/LoggerAppenderSayTest.php
+++ tests/unit/appenders/LoggerAppenderSayTest.php
@@ -3,12 +3,17 @@
 /**
  * Special for mac cool appender!
  */
-class LoggerAppenderSayTest extends PHPUnit_Framework_TestCase
+class LoggerAppenderSayTest extends BaseLoggerTestCase
 {
+    protected $backupGlobals = true;
+
     public function testWrite()
     {
+        $GLOBALS['say']=null;
+        $this->mockFunction('system', '$c', '$GLOBALS["say"]=$c;return "";');
         $appender = new LoggerAppenderSay();
         $return = $appender->write(1, 'fuck, web site is down!');
         $this->assertTrue($return !== false);
+        $this->assertEquals('say \'fuck, web site is down!\'', $GLOBALS['say']);
     }
 }
