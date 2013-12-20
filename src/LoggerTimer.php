4baaa9af5ad41d9aa8099c9c378db99b402260dc
Корчевский Алексей <a.korchevsky@corp.mail.ru>
2013-12-20 15:54:41 +0400

    fix logger timer

--- src/LoggerTimer.php
+++ src/LoggerTimer.php
@@ -24,37 +24,37 @@ class LoggerTimer
 
     public function trace($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::TRACE, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::TRACE, $this->render($message, microtime(true)), $throwable);
     }
 
     public function debug($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::DEBUG, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::DEBUG, $this->render($message, microtime(true)), $throwable);
     }
 
     public function info($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::INFO, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::INFO, $this->render($message, microtime(true)), $throwable);
     }
 
     public function warn($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::WARN, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::WARN, $this->render($message, microtime(true)), $throwable);
     }
 
     public function error($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::ERROR, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::ERROR, $this->render($message, microtime(true)), $throwable);
     }
 
     public function fatal($message, Exception $throwable = null)
     {
-        $this->logger->log(Logger::FATAL, $this->render($message, microtime(true), $throwable));
+        $this->logger->log(Logger::FATAL, $this->render($message, microtime(true)), $throwable);
     }
 
     public function log($level, $message, Exception $throwable = null)
     {
-        $this->logger->log($level, $this->render($message, microtime(true), $throwable));
+        $this->logger->log($level, $this->render($message, microtime(true)), $throwable);
     }
 
     public function render($message, $end)
