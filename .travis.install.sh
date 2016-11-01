223fd96a1a2b0d50791a02d70e6df287c5cb8e14
Mougrim <rinat@mougrim.ru>
2016-11-01 09:56:12 +0300

    remove xdebug if coverage disabled

--- .travis.install.sh
+++ .travis.install.sh
@@ -4,5 +4,14 @@ PATH=`dirname "$(readlink -f "$0")"`/tests/bin:$PATH
 [ -d /tmp/mocks/ ] || mkdir /tmp/mocks/
 
 if [ `./.travis.check-php-version-coverage.php` -eq "1" ]; then
+  echo 'enable coverage'
   echo "xdebug.coverage_enable=On" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+else
+  XDEBUG_INI="/home/travis/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini"
+  if [ -f "$XDEBUG_INI" ]; then
+    echo 'remove xdebug config'
+    phpenv config-rm xdebug.ini
+  else
+    echo "xdebug config isn't exists"
+  fi
 fi
